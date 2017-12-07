f65c6c49d921e4f493a05be52a817d84a01465c7
moroz69off <moroz69off@gmail.com>
2017-12-07 21:06:43 +0300

    Added 2 element

---
+++ bin/Debug/apps/mrzselenbro/mrzselenbro/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("seleniumbrowser")]
+[assembly: AssemblyDescription("Программка-шутка-бот")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("moroz69off")]
+[assembly: AssemblyProduct("seleniumbrowser")]
+[assembly: AssemblyCopyright("Copyright moroz69off©  2017")]
+[assembly: AssemblyTrademark("moroz69off™")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(true)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("b30ba398-6f2b-4bdc-86e7-a8598ab19e0b")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
