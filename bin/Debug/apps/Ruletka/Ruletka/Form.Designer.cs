a2a0a3d3351b3f0ea8c0586701a3b00f7e7b98b8
Морозов Юрий <moroz69off@outlook.com>
2020-01-16 19:14:51 +0300

    Перенесён проект "Рулетка"

---
+++ bin/Debug/apps/Ruletka/Ruletka/Form.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace triOtsenki
+{
+    partial class GForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GForm));
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::triOtsenki.Properties.Resources.img3;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox3.Location = new System.Drawing.Point(504, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(240, 237);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::triOtsenki.Properties.Resources.img2;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(258, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(240, 237);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::triOtsenki.Properties.Resources.img1;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(240, 237);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button
+            // 
+            this.button.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.25F);
+            this.button.Location = new System.Drawing.Point(12, 255);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(732, 66);
+            this.button.TabIndex = 3;
+            this.button.Text = "Knopka";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 99;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 123;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // GForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 333);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "GForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tri kartinki";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ bin/Debug/apps/Ruletka/Ruletka/Form.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace triOtsenki
+{
+    public partial class GForm : Form
+    {
+        private int nr = 1;
+        private int ruletka1, ruletka2, ruletka3;
+        Random rand = new Random();
+        public GForm()
+        {
+            InitializeComponent();
+            timer1.Stop();
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            nr++;
+            ChengePic(nr);
+        }
+
+        private void ChengePic(int nr)
+        {
+            if (nr%3==0)
+	        {
+                pictureBox1.BackgroundImage = Properties.Resources.img1;
+                pictureBox2.BackgroundImage = Properties.Resources.img2;
+                pictureBox3.BackgroundImage = Properties.Resources.img3;
+            }
+            else if (nr % 3 == 1)
+            {
+                pictureBox1.BackgroundImage = Properties.Resources.img2;
+                pictureBox2.BackgroundImage = Properties.Resources.img3;
+                pictureBox3.BackgroundImage = Properties.Resources.img1;
+            }
+            else if (nr%3==2)
+            {
+                pictureBox1.BackgroundImage = Properties.Resources.img3;
+                pictureBox2.BackgroundImage = Properties.Resources.img1;
+                pictureBox3.BackgroundImage = Properties.Resources.img2;
+            }
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            nr = 0;
+            if (!timer.Enabled) 
+            {
+                timer.Start();
+                timer1.Stop();
+            } else
+            { 
+                timer.Stop();
+                ruletka1 = rand.Next(2, 8);
+                ruletka2 = rand.Next(2, 8);
+                ruletka3 = rand.Next(2, 8);
+                timer1.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            nr++;
+            ChengePic(nr);
+            if (nr > ruletka1)
+            {
+                pictureBox1.Invalidate(true);
+                if (ruletka1 % 3 == 0) pictureBox1.BackgroundImage = Properties.Resources.img1;
+                else if (ruletka1 % 3 == 1) pictureBox1.BackgroundImage = Properties.Resources.img2;
+                else if (ruletka1 % 3 == 2) pictureBox1.BackgroundImage = Properties.Resources.img3;
+                
+            } 
+            if (nr > ruletka2)
+            {
+                pictureBox2.Invalidate(true);
+                if (ruletka2 % 3 == 0) pictureBox2.BackgroundImage = Properties.Resources.img1;
+                else if (ruletka2 % 3 == 1) pictureBox2.BackgroundImage = Properties.Resources.img2;
+                else if (ruletka2 % 3 == 2) pictureBox2.BackgroundImage = Properties.Resources.img3;
+            }
+            if (nr > ruletka3)
+            {
+                pictureBox3.Invalidate(true);
+                if (ruletka3 % 3 == 0) pictureBox3.BackgroundImage = Properties.Resources.img1;
+                else if (ruletka3 % 3 == 1) pictureBox3.BackgroundImage = Properties.Resources.img2;
+                else if (ruletka3 % 3 == 2) pictureBox3.BackgroundImage = Properties.Resources.img3;
+            }
+
+        }
+    }
+}
