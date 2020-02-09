31008e0ec89469cc2af98ee9daad8d22c04e40be
Морозов Юрий <moroz69off@outlook.com>
2020-02-10 01:38:46 +0300

    del red

--- bin/Debug/apps/FastDoIt/FastDoIt/SaveProfileForm.cs
+++ bin/Debug/apps/FastDoIt/FastDoIt/SaveProfileForm.cs
@@ -1,13 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Configuration;
 
 namespace MrzFastDoIt
 {
@@ -16,14 +8,15 @@ namespace MrzFastDoIt
         MainForm MF;
         BindSetting BindSetting;
         string profileName;
-        
-        
+
+
         public SaveProfileForm()
         {
             InitializeComponent();
             MF = new MainForm();
+            Control.ControlCollection controls = MF.Controls;
             BindSetting = new BindSetting();
-            
+
         }
 
         private void button_saveProfileName_Click(object sender, EventArgs e)
