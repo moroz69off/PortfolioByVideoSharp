6d9f6dd2e994f4539206aecbb84a302519f48703
Морозов Юрий <moroz69off@gmail.com>
2020-09-29 08:55:30 +0300

    is ffmpeg

--- bin/Debug/apps/MRZVideoTr/Program.cs
+++ bin/Debug/apps/MRZVideoTr/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,29 @@ namespace MRZVideoTr
 {
     class Program
     {
+        private static string appPath;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World");
+
+            appPath = Environment.CurrentDirectory;
+            // потребуется ffmpeg.exe
+            //        Links
+            // https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-full.zip
+            // https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip
+            // https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-full-shared.zip
+
+            if (!Directory.Exists(appPath + "/ffmpeg.exe"))
+            {
+                // dounload ffmpeg.exe
+                Console.WriteLine(
+                    "Need ffmpeg.exe\nHe is not there...\n"+
+                    "Download here:\n"+
+                    "https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-full.zip");
+            }
+
+
+
             Console.ReadLine();
         }
     }
