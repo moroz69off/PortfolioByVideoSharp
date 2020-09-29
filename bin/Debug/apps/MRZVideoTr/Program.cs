a3eead0e1afb35d6434d6a85178f2b978fcdce37
Морозов Юрий <moroz69off@gmail.com>
2020-09-29 09:48:00 +0300

    Audio Ok

--- bin/Debug/apps/MRZVideoTr/Program.cs
+++ bin/Debug/apps/MRZVideoTr/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -9,29 +10,50 @@ namespace MRZVideoTr
 {
     class Program
     {
-        private static string appPath;
+        static string appPath;
+        static string pathToVideo;
+        static string ffArgs;
+        private static string videoName;
+
         static void Main(string[] args)
         {
+            Console.Title = "MRZVideoTr";
             Console.WriteLine("Hello, World");
 
             appPath = Environment.CurrentDirectory;
+
+            pathToVideo = "D:/Cubebrush - Dragon Knight - Fantasy character full course (2017)/01/01-01-GET-STARTED.mp4";
+
+            char[] c = new char[] { '/', '\\' };
+            int videoNameStartIndex = pathToVideo.LastIndexOfAny(c) + 1;
+            videoName = pathToVideo.Substring(videoNameStartIndex);
+            ffArgs = " -i \"" + pathToVideo + "\" -c:a copy " + videoName + ".aac";
+
             // потребуется ffmpeg.exe
             //        Links
             // https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-full.zip
             // https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip
             // https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-full-shared.zip
 
-            if (!Directory.Exists(appPath + "/ffmpeg.exe"))
+            bool isFF = File.Exists(appPath + "\\ffmpeg.exe");
+
+            if (!isFF)
             {
-                // dounload ffmpeg.exe
                 Console.WriteLine(
                     "Need ffmpeg.exe\nHe is not there...\n"+
                     "Download here:\n"+
                     "https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-full.zip");
+                Console.WriteLine("For exit press \"Enter\" key");
+                Console.ReadLine();
+                return;
             }
 
+            Process i = Process.Start("ffmpeg.exe", ffArgs);
+            int processID = i.Id;
+            Console.WriteLine(i.ProcessName);
+            processID++;
 
-
+            Console.WriteLine("Press \"Enter\" key, user!");
             Console.ReadLine();
         }
     }
