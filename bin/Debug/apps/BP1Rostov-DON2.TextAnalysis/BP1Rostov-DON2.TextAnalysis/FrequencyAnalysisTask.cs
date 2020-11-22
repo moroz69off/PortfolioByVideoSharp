75a90fc220b1ee16dc187f326eb7222b61790a1b
Морозов Юрий <moroz69off@gmail.com>
2020-11-22 22:49:46 +0300

    added project files

---
+++ bin/Debug/apps/BP1Rostov-DON2.TextAnalysis/BP1Rostov-DON2.TextAnalysis/FrequencyAnalysisTask.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TextAnalysis
+{
+    internal static class FrequencyAnalysisTask
+    {
+        public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text)
+        {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+
+            Dictionary<string, int> frequency = new Dictionary<string, int>();
+
+            List<string> allx2gramas = new List<string>(Get2gramas(text));
+
+            foreach (var item in allx2gramas)
+            {
+                if (!frequency.ContainsKey(item))
+                    frequency.Add(item, 1);
+                else
+                    frequency[item]++;
+            }
+
+            List<string> allx3gramas = new List<string>(Get3gramas(text));
+
+            foreach (var item in allx3gramas)
+            {
+                if (!frequency.ContainsKey(item))
+                    frequency.Add(item, 1);
+                else
+                    frequency[item]++;
+            }
+
+            for (int i = 0; i < frequency.Count; i++)
+            {
+
+            }
+
+            return result;
+        }
+
+        private static List<string> Get3gramas(List<List<string>> text)
+        {
+            List<string> result = new List<string>();
+            foreach (var item in text)
+            {
+                for (int i = 0; i < item.Count - 2; i++)
+                    result.Add(item[i] + " "
+                        + item[i + 1] + " "
+                        + item[i + 2]);
+            }
+
+            result.Sort();
+
+            return result;
+        }
+
+        private static List<string> Get2gramas(List<List<string>> text)
+        {
+            List<string> result = new List<string>();
+
+            foreach (var item in text)
+            {
+                for (int i = 0; i < item.Count - 1; i++)
+                    result.Add(item[i] + " "
+                        + item[i + 1]);
+            }
+
+            result.Sort();
+
+            return result;
+        }
+    }
+}
---
+++ bin/Debug/apps/BP1Rostov-DON2.TextAnalysis/BP1Rostov-DON2.TextAnalysis/SentencesParserTask.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextAnalysis
+{
+    static class SentencesParserTask
+    {
+        public static List<List<string>> ParseSentences(string text)
+        {
+            char[] sentenceSeparators = new char[]{'.', '!', '?', ';', ':', '(', ')' };
+            string[] sentences = text
+                .Split(sentenceSeparators,
+                StringSplitOptions.RemoveEmptyEntries);
+            return ParseAndAddWordsToLists(sentences);
+        }
+
+        private static List<List<string>> ParseAndAddWordsToLists(string[] sentences)
+        {
+            List<List<string>> sentencesList = new List<List<string>>();
+            char[] wordSeparators = new char[] { ' ', ',' };
+            for (int i = 0; i < sentences.Length; i++)
+            {
+                List<string> words = SentencesParse(sentences, wordSeparators, i);
+                sentencesList.Add(words);
+            }
+            return RemoteEmptyWords(sentencesList);
+        }
+
+        private static List<List<string>> RemoteEmptyWords(List<List<string>> sentencesList)
+        {
+            List<List<string>> list = new List<List<string>>(sentencesList.Count / 8);
+            foreach (var item in sentencesList)
+                if (item.Count != 0)
+                    list.Add(item);
+            return list;
+        }
+
+        private static List<string> SentencesParse(string[] sentences, 
+            char[] wordSeparators, int i)
+        {
+            StringBuilder sb = new StringBuilder();
+            char[] chars = sentences[i].ToLower().ToCharArray();
+            for (int l = 0; l < chars.Length; l++)
+            {
+                if (char.IsLetter(chars[l]) || chars[l] == '\'')
+                    sb.Append(chars[l]);
+                else
+                    sb.Append(' ');
+            }
+            sentences[i] = sb.ToString().Trim(wordSeparators);
+            List<string> words = new List<string>(sentences[i]
+                .Split(
+                wordSeparators,
+                StringSplitOptions.RemoveEmptyEntries));
+            words.RemoveAll(x => x.Length == 0);
+            return words;
+        }
+    }
+}
