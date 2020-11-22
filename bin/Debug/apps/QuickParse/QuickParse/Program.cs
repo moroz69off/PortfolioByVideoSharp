d79235b4207ed05c3563d9157b4634db45c52d40
Морозов Юрий <moroz69off@gmail.com>
2020-11-22 22:14:02 +0300

    quick parser return

--- bin/Debug/apps/QuickParse/QuickParse/Program.cs
+++ bin/Debug/apps/QuickParse/QuickParse/Program.cs
@@ -1,25 +1,20 @@
 ﻿using System;
-using Algorithms;
-using CMDLine;
-using TI;
-using TI.Declarator;
-using TI.Declarator.ParserCommon;
 
 namespace QuickParse
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+	class Program
+	{
+		static void Main(string[] args)
+		{
 
-            Console.ReadLine();
-        }
-    }
+			Console.ReadLine();
+		}
+	}
 
-    interface IQDoc
-    {
-        string Path { get; set; }
-        bool IsHappily { get; }
-        string Result { get; }
-    }
+	interface IQDoc
+	{
+		string Path { get; set; }
+		bool IsHappily { get; }
+		string Result { get; }
+	}
 }
