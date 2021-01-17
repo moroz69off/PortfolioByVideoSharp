2f7d522f443360364cb818d37b96f927befd2670
moroz69off <moroz69off@gmail.com>
2021-01-17 05:13:39 +0300

    new project

---
+++ bin/Debug/apps/BrouserAutomation/BrouserAutomation/MainForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace BrouserAutomation
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonPromo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Left;
+            this.webBrowser.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(800, 680);
+            this.webBrowser.TabIndex = 0;
+            this.webBrowser.Url = new System.Uri("https://gaming.pringles.com/ru_RU/entercode", System.UriKind.Absolute);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.textBox1.Location = new System.Drawing.Point(806, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(282, 24);
+            this.textBox1.TabIndex = 1;
+            // 
+            // buttonPromo
+            // 
+            this.buttonPromo.Location = new System.Drawing.Point(806, 42);
+            this.buttonPromo.Name = "buttonPromo";
+            this.buttonPromo.Size = new System.Drawing.Size(282, 23);
+            this.buttonPromo.TabIndex = 2;
+            this.buttonPromo.Text = "APPLY PROMOCODE";
+            this.buttonPromo.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1100, 680);
+            this.Controls.Add(this.buttonPromo);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.webBrowser);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Broser automation";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonPromo;
+    }
+}
+
